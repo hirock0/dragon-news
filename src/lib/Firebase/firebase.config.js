36d372523686157de0731b0b2dbb122059d031c5
hirock0 <hirockdutta0@gmail.com>
2025-02-07 22:50:53 +0600

    vercel file added

--- src/lib/Firebase/firebase.config.js
+++ src/lib/Firebase/firebase.config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   authDomain: import.meta.VITE_AUTH_DOMAIN,
   projectId: import.meta.VITE_PROJECT_ID,
   storageBucket: import.meta.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.VITE_MESSAGING - SENDER_ID,
+  messagingSenderId: import.meta.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.VITE_APP_ID,
 };
 
